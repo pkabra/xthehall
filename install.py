7c692b75976da5a332d6247e97dc202210401c11
pkabra <pratik@kabra.com>
2015-02-05 20:25:03 -0500

    Fix installer

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 from subprocess import call
 
 def bash_profile_settings():
-	call("export PATH=/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH", shell=True)
+	call("echo 'export PATH=/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH' > ~/.bash_profile", shell=True)
 
 def install_virtualenv():
 	call("sudo pip install virtualenv", shell=True)
