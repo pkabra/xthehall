a58ebadb22b5649e4d4c980096785ce9e50f7c78
pkabra <pratik@kabra.com>
2015-02-05 20:17:53 -0500

    Update the install

--- install.py
+++ install.py
@@ -1,6 +1,10 @@
 from subprocess import call
 
+def bash_profile_settings():
+	call("export PATH=/Applications/Postgres.app/Contents/Versions/9.4/bin:$PATH", shell=True)
+
 def install_virtualenv():
 	call("sudo pip install virtualenv", shell=True)
 
+bash_profile_settings()
 install_virtualenv()
