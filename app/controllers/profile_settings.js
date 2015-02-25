b4a00a91443caa79b7cbbb1c4298c76885b754a1
Michael Wang <mwwang@umich.edu>
2015-02-25 17:12:05 -0500

    Working profile settings

--- app/controllers/profile_settings.js
+++ app/controllers/profile_settings.js
@@ -1,56 +1,56 @@
 
 angular.module('XtheHall')
 .directive('ngEnter', function() {
-        return function(scope, element, attrs) {
-            element.bind("keydown keypress", function(event) {
-                if(event.which === 13) {
-                        scope.$apply(function(){
-                                scope.$eval(attrs.ngEnter);
-                        });
-                        
-                        event.preventDefault();
-                }
-            });
-        };
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function(){
+          scope.$eval(attrs.ngEnter);
+        });
+
+        event.preventDefault();
+      }
+    });
+  };
 })
 .controller('ProfileSettingsController', function($scope, ProfileService) {
 
-      var fileInput = document.getElementById('image-input');
-      var image = document.getElementById('image-container');
-      $scope.hospital_list = ["Mott Children's Hospital", "Univ. of Michigan Hospital", "Ohio State Hospital"];
-      $scope.interest_list = ["Soccer", "Football", "Basketball"];
-      $scope.nickname_width = "width: 50px;";
-
-    $scope.click_nickname = function(){
-
-      console.log("CLICK!");
-
-      final_transcript = '';
-      recognition.start();
+  var fileInput = document.getElementById('image-input');
+  var image = document.getElementById('image-container');
 
 
+  $scope.click_nickname = function(){
+
+    console.log("CLICK!");
+
+    final_transcript = '';
+    recognition.start();
+
+
+  }
+
+  $scope.image_upload = function(){
+
+    var fileUrl = window.URL.createObjectURL(fileInput.files[0]);
+    image.src = fileUrl;
+    console.log(JSON.stringify(typeof(fileInput)));
+    console.log(fileUrl);
+    console.log(JSON.stringify(fileInput.files[0]));
+
+
+    fullPath = document.getElementById('image-input').value;
+
+    if (fullPath) {
+      var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+      var filename = fullPath.substring(startIndex);
+      if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+        filename = filename.substring(1);
+      }
     }
 
-    $scope.image_upload = function(){
-
-          var fileUrl = window.URL.createObjectURL(fileInput.files[0]);
-          image.src = fileUrl;
-          console.log(fileUrl);
-
-
-          fullPath = document.getElementById('image-input').value;
-
-          if (fullPath) {
-            var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
-            var filename = fullPath.substring(startIndex);
-            if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
-              filename = filename.substring(1);
-            }
-          }
-
-          $scope.avatar_img_name = filename;
-          $scope.$apply();
-    };
+    $scope.avatar_img_name = filename;
+    $scope.$apply();
+  };
 
 
 //Change input width to match width of characters.
@@ -74,48 +74,67 @@ $scope.click = function(index)
 
 
 
-    if (!('webkitSpeechRecognition' in window)) {
-        console.log("DAMN");
-    } else {
-      console.log("COOL");
+if (!('webkitSpeechRecognition' in window)) {
+  console.log("DAMN");
+} else {
+  console.log("COOL");
 
-      var recognition = new webkitSpeechRecognition();
-      recognition.continuous = false;
-      recognition.interimResults = false;
+  var recognition = new webkitSpeechRecognition();
+  recognition.continuous = false;
+  recognition.interimResults = false;
 
-      recognition.onstart = function() {  };
-      recognition.onresult = function(event) { $scope.nickname = event.results[event.results.length-1][0].transcript; $scope.nickname_width = $scope.onWrite($scope.nickname); console.log($scope.nickname); $scope.$apply();};
-      recognition.onerror = function(event) {  };
-      recognition.onend = function() {  };
+  recognition.onstart = function() {  };
+  recognition.onresult = function(event) { $scope.nickname = event.results[event.results.length-1][0].transcript; $scope.nickname_width = $scope.onWrite($scope.nickname); console.log($scope.nickname); $scope.$apply();};
+  recognition.onerror = function(event) {  };
+  recognition.onend = function() {  };
 
-    }
-
-
-      function init() {
-
-        // ProfileService.getNickname();
+}
 
 
-        // $scope.nickname = ProfileService.getNickname();
-        $scope.nickname = "Michael Wang";
-        $scope.nickname_width = $scope.onWrite($scope.nickname);
-        $scope.hospital_list = JSON.parse('["Mott Children\'s Hospital", "Univ. of Michigan Hospital", "Ohio State Hospital"]');
-        $scope.interest_list = JSON.parse('["Soccer", "Football", "Basketball"]');
-      
-      }
+function init() {
 
-      init();
+  console.log(ProfileService);
 
-      
-
-      $scope.save = function() {
-
-        console.log("Save");
-        console.log($scope.nickname);
-        console.log(JSON.stringify($scope.interest_list));
-        console.log(document.getElementById('hospital_select').value);
-      };
+  $scope.hospital_list = JSON.parse('["Sinai-Grace Hospital", "Henry Ford Hospital", "Beaumont Hospital", "Oakwood Heritage Hospital"]');
+  $scope.save_click = false;
 
 
+  try {
+    $scope.nickname = ProfileService.getNickname();
+    $scope.nickname_width = $scope.onWrite($scope.nickname);
+    $scope.interest_list = JSON.parse(ProfileService.getInterest());
+    $scope.hospital_select = ProfileService.getHospital_info();
+    image.src = ProfileService.getImage();
+    console.log("Successfully pulled values.");
+
+  }
+  catch(err) {
+
+    $scope.nickname = "";
+    $scope.nickname_width = "width: 50px;";
+    $scope.interest_list = [];
+
+    console.log("Couldn't pull values, populating with default.");
+  }
+
+  console.log("Done");
+}
+
+init();
+
+$scope.save = function() {
+
+  console.log("Save");
+  console.log($scope.nickname);
+  console.log(JSON.stringify($scope.interest_list));
+  console.log(JSON.stringify($scope.hospital_select));
+
+  ProfileService.setNickname($scope.nickname);
+  ProfileService.setInterest(JSON.stringify($scope.interest_list));
+  ProfileService.setHospital_info($scope.hospital_select);
+  ProfileService.setImage(fileInput.files);
+};
+
+document.getElementById('hospital_select').value = "Beaumont Hospital";
 
 });
