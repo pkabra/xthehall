600af047c27f66532ab5fd28d8ed64c868562811
Adam Gorman <Adam@0587378028.wireless.umich.net>
2015-04-05 20:50:44 -0400

    Settings Page Redesign
    (Functionality still in progress)

--- app/controllers/profile_settings.js
+++ app/controllers/profile_settings.js
@@ -20,14 +20,10 @@ angular.module('XtheHall')
 
 
   $scope.click_nickname = function(){
-
-    console.log("CLICK!");
-
-    final_transcript = '';
     recognition.start();
 
 
-  }
+  };
 
   $scope.image_upload = function(){
 
@@ -52,14 +48,6 @@ angular.module('XtheHall')
     $scope.$apply();
   };
 
-
-//Change input width to match width of characters.
-$scope.onWrite = function (text)
-{
-  width = "width: " + (text.length * 22 + 50).toString() + "px;";
-  return width;
-};
-
 $scope.add_interest = function(item, interest_list)
 {
   $scope.interest_list.push(item);
@@ -75,18 +63,20 @@ $scope.click = function(index)
 
 
 if (!('webkitSpeechRecognition' in window)) {
-  console.log("DAMN");
+  console.log("speech webkit not found");
 } else {
-  console.log("COOL");
-
   var recognition = new webkitSpeechRecognition();
   recognition.continuous = false;
   recognition.interimResults = false;
 
-  recognition.onstart = function() {  };
-  recognition.onresult = function(event) { $scope.nickname = event.results[event.results.length-1][0].transcript; $scope.nickname_width = $scope.onWrite($scope.nickname); console.log($scope.nickname); $scope.$apply();};
-  recognition.onerror = function(event) {  };
-  recognition.onend = function() {  };
+  recognition.onstart = function() { };
+  recognition.onresult = function(event) {
+      $scope.nickname = event.results[event.results.length-1][0].transcript;
+      console.log($scope.nickname);
+      $scope.$apply();
+  };
+  recognition.onerror = function(event) { };
+  recognition.onend = function() { };
 
 }
 
@@ -103,26 +93,13 @@ function init() {
 
   try {
     $scope.nickname = ProfileService.getNickname();
-    console.log("1");
-    $scope.nickname_width = $scope.onWrite($scope.nickname);
-    console.log("2");
-
-
-
     $scope.interest_list = ProfileService.getInterest();
-    console.log("3");
-    
-    
     $scope.hospital_select = ProfileService.getHospital_info();
-    console.log("4");
     image.src = ProfileService.getImage();
     console.log("Successfully pulled values.");
-
   }
   catch(err) {
-
     $scope.nickname = "";
-    $scope.nickname_width = "width: 50px;";
     $scope.interest_list = [];
 
     console.log("Couldn't pull values, populating with default.");
@@ -134,17 +111,18 @@ function init() {
 init();
 
 $scope.save = function() {
-
   console.log("Save");
   console.log($scope.nickname);
   console.log(JSON.stringify($scope.interest_list));
   console.log(JSON.stringify($scope.hospital_select));
+    debugger;
 
   ProfileService.setNickname($scope.nickname);
   ProfileService.setInterest($scope.interest_list);
   ProfileService.setHospital_info($scope.hospital_select);
   ProfileService.setImage(fileInput.files);
 };
+        $('#interestSelect').select2();
 
 document.getElementById('hospital_select').value = "Beaumont Hospital";
 
